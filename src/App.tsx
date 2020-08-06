2abf3d4772ff0029cdffb00f35f662c063df7172
EmilyDeLisle <emily.delisle@gmail.com>
2020-08-06 16:35:35 -0700

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 import { Header } from './components'
 
 function App() {
   return (
     <div>
       <Header />
+      <div>
+        <Typography>
+          camera soul-delay -space woman -space corrupted stimulate fetishism. bomb beef noodles
+          kanji man franchise girl cyber- math-. range-rover artisanal man uplink smart- sub-orbital
+          Kowloon -space. human stimulate media rifle into rifle otaku marketing. numinous receding
+          singularity marketing rain claymore mine footage singularity. realism receding tank-traps
+          urban 3D-printed human systema claymore mine. youtube face forwards motion dome assassin
+          receding augmented reality spook. semiotics dome marketing tube systemic chrome ablative
+          A.I.. dome systemic BASE jump man Legba systemic spook order-flow.
+        </Typography>
+        <Typography>
+          long-chain hydrocarbons grenade dolphin table pistol industrial grade modem corrupted.
+          tank-traps disposable bridge motion shanty town cardboard convenience store woman. pre-
+          Kowloon boy ablative alcohol neon meta- bicycle. girl ablative urban fetishism narrative
+          into spook wristwatch. lights bomb chrome digital saturation point DIY euro-pop digital.
+          hotdog media spook drone carbon hotdog decay warehouse. savant stimulate denim franchise
+          man disposable crypto- rebar. knife chrome order-flow augmented reality chrome Legba media
+          apophenia. sunglasses j-pop paranoid faded geodesic DIY office pistol.
+        </Typography>
+        <Typography>
+          pen cardboard sensory industrial grade uplink fetishism boy office. market smart- plastic
+          tanto order-flow pen garage singularity. rain nano- concrete sunglasses augmented reality
+          shanty town sprawl otaku. geodesic ablative cyber- lights network Legba artisanal vinyl.
+          hacker hotdog assassin singularity city network -ware corporation. garage tanto film
+          systemic paranoid wristwatch digital garage. camera dolphin sunglasses alcohol franchise
+          augmented reality futurity singularity. math- corporation apophenia chrome -space receding
+          math- shanty town. cartel network stimulate neon lights physical courier claymore mine.
+        </Typography>
+      </div>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,28 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    header: {
+      color: theme.palette.primary.contrastText,
+    },
+    text: {
+      fontWeight: 500,
+      textShadow: '2px 2px 6px rgba(0, 0, 0, 0.8)',
+    },
+    name: {
+      fontFamily: 'Leckerli One, cursive',
+      color: '#EA1E5A',
+    },
+  })
+)
 
 export const Header = () => {
+  const classes = useStyles()
   return (
-    <div className="header">
+    <div className={`${classes.header} ${classes.text} header`}>
       <AppBar>
         <Toolbar>
           <Typography>mle</Typography>
@@ -14,8 +32,10 @@ export const Header = () => {
       <Toolbar />
       <div className="header__contents">
         <div>
-          <Typography variant="h1">Hi! I'm Emily DeLisle.</Typography>
-          <Typography variant="h5">
+          <Typography className={classes.text} variant="h1" gutterBottom>
+            Hi! I'm <span className={classes.name}>Emily DeLisle</span>.
+          </Typography>
+          <Typography className={classes.text} variant="h5">
             I'm a (soon-to-be) new graduate of the Computer Systems Technology program at the
             British Columbia Institute of Technology. I'm currently seeing a job as a software
             developer.
